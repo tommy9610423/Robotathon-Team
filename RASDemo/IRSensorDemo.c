8947e47bd2124521eaadfe043eee78b1dedbf566
UTDavin <dsiu@utexas.edu>
2014-10-19 19:53:08 -0500

    Update IRSensorDemo.c

--- RASDemo/IRSensorDemo.c
+++ RASDemo/IRSensorDemo.c
@@ -6,6 +6,24 @@
 static tADC *adc[4];
 static tBoolean initialized = false;
 
+// function : y = 21.1x + .211111 .  y = output. x = inverted distance (cm)
+/*
+                y = 21618x + 216;
+                y>>10;
+                
+               input /4095 * 3.3 = 21.1x + .21111
+               
+            21.1111 /  ( input / 4096 * 3.3 - .2111 )  = distance
+            
+                  ( 21618  /  ( input / 4  * 3379 -216)) >> 10 = distance
+               
+               
+               
+               
+           */     
+
+// 
+
 void initIRSensor(void) {
     // don't initialize this if we've already done so
     if (initialized) {
@@ -15,12 +33,29 @@ void initIRSensor(void) {
     initialized = true;
 
     // initialize 4 pins to be used for ADC input
-    adc[0] = InitializeADC(PIN_D0);
-    adc[1] = InitializeADC(PIN_D1);
-    adc[2] = InitializeADC(PIN_D2);
-    adc[3] = InitializeADC(PIN_D3);
+    adc[0] = InitializeADC(PIN_D0); // Left IR
+    adc[1] = InitializeADC(PIN_D1); // Front IR
+    adc[2] = InitializeADC(PIN_D2); // Right IR
+//    adc[3] = InitializeADC(PIN_D3);
 }
 
+
+void readRight(void){
+ADCRead(adc[2]);
+
+    
+}
+
+void readLeft(void){
+ADCRead(adc[0]);
+    
+}
+
+void readFront(void){
+ADCRead(adc[1]);
+    
+}
+/*
 void IRSensorDemo(void) {
     Printf("Press any key to quit\n");
         
@@ -37,4 +72,4 @@ void IRSensorDemo(void) {
   
     Printf("\n");
 }
-
+*/
