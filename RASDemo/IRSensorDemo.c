d4105252855193028691533a6e8c91760eb6f1a2
UTDavin <dsiu@utexas.edu>
2014-10-22 21:02:38 -0500

    Update IRSensorDemo.c

--- RASDemo/IRSensorDemo.c
+++ RASDemo/IRSensorDemo.c
@@ -11,18 +11,13 @@ static tBoolean initialized = false;
                 y = 21618x + 216;
                 y>>10;
                 
-               input /4095 * 3.3 = 21.1x + .21111
+               input * 3.3 = 21.1x + .21111
                
             21.1111 /  ( input  * 3.3 - .2111 )  = distance
             
                   ( 21618  /  ( input * 3379 -216)) >> 10 = distance
-               
-               
-               
-               
            */     
 
-// 
 
 void initIRSensor(void) {
     // don't initialize this if we've already done so
@@ -41,30 +36,87 @@ void initIRSensor(void) {
 
 
 float readRight(void){
-float input = ADCRead(adc[2]);
-if(input < .15){return 1000;} // .15 = 15% of 3.3V = approx. out of bounds distance (80cm)
-float cmdist = (( 21618  /  ( input * 3379 -216)) >> 10);
+    float input = ADCRead(adc[2]);
+    if(input < .15){return 1000;} // .15 = 15% of 3.3V = approx. out of bounds distance (80cm)
+    float cmdist = (( 21618  /  ( input * 3379 -216)) >> 10);
+    cmdist -= 10; // if sensors are placed 10cm inward, offset 10cm
     return cmdist;
 }
 
 float readLeft(void){
-float input = ADCRead(adc[0]);
-if(input < .15){return 1000;} // .15 = 15% of 3.3V = approx. out of bounds distance (80cm)
-float cmdist = (( 21618  /  ( input * 3379 -216)) >> 10);
+    float input = ADCRead(adc[0]);
+    if(input < .15){return 1000;} // .15 = 15% of 3.3V = approx. out of bounds distance (80cm)
+    float cmdist = (( 21618  /  ( input * 3379 -216)) >> 10);
+    cmdist -= 10; // if sensors are placed 10cm inward, offset 10cm
     return cmdist;
 }
 
 float readFront(void){
-float input = ADCRead(adc[1]);
-if(input < .15){return 1000;} // .15 = 15% of 3.3V = approx. out of bounds distance (80cm)
-float cmdist = (( 21618  /  ( input * 3379 -216)) >> 10);
+    float input = ADCRead(adc[1]);
+    if(input < .15){return 1000;} // .15 = 15% of 3.3V = approx. out of bounds distance (80cm)
+    float cmdist = (( 21618  /  ( input * 3379 -216)) >> 10);
+    cmdist -= 10; // if sensors are placed 10cm inward, offset 10cm
     return cmdist;
     
 }
 
 
 void wallFollow(char x[], float dist){
-
+    if(x[0] == 'r'){
+        while(readRight()!= 1000 && readFront() >=15){ // and while no line is detected
+            current = readRight();
+            if(readRight()-dist >=2){ // drifted to the left
+                swivelRight();
+                while(readRight()-dist >=2){}
+                Forward();
+            }
+                                                             
+            if(dist-readRight() >=2){ // drifted to the right
+                swivelLeft();
+                while(dist-readRight() >=2){}
+                Forward();
+            }
+                                                
+        }
+        
+                                // insert 90 degree left turn later
+       /* if(line detected){return;}
+        if(readFront() <= 15){
+            float t1 = GetTime();
+            float t2 = GetTime();
+            swivelRight();
+            while(t2-t1 <= 1){    // 
+                t2 = GetTime();
+            }
+        } */
+    }
+                
+        else if(x[0] == 'l'){
+            while(readLeft() != 1000 && readFront()>=15){ // and while no line is detected
+                if(readLeft()-dist >=2){ // drifted to the left
+                    swivelRight();
+                    while(readRight()-dist >=2){}
+                    Forward();
+                }
+                                                             
+                if(dist-readRight() >=2){ // drifted to the right
+                    swivelLeft();
+                    while(dist-readRight() >=2){}
+                    Forward();
+                }
+            }
+             /* if(line detected){return;}
+            if(readFront() <= 15){
+                float t1 = GetTime();
+                float t2 = GetTime();
+                swivelLeft();
+                while(t2-t1 <= 1){    // 
+                    t2 = GetTime();
+                }
+            } */
+        }
+    
+        // insert 90 degree right turn later
 }
 /*
 void IRSensorDemo(void) {
